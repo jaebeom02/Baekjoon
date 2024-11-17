5d8f189a91b92d55a29c9d2309f1fd14e2524c2d
jaebeom02 <gsxxx007@naver.com>
2024-11-18 00:16:56 +0900

    백준 소스코드를 굳이 올려야 할까

---
+++ Baekjoon/11053.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int n;
+	cin >> n;
+	int arr[1001];
+	int dp[1001];
+
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		dp[i] = 1;
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j < i; j++) {
+			if (arr[i] > arr[j]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+
+	sort(dp, dp + n + 1);
+	cout << dp[n];
+
+	return 0;
+}
